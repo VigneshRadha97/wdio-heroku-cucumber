52713e5bc072bda0997b7b9a4a3a5963d77c7eba
VigneshRadha97 <vignesh19970730@gmail.com>
2022-10-12 11:40:59 +0530

    pom update

--- features/step-definitions/steps.js
+++ features/step-definitions/steps.js
@@ -6,8 +6,6 @@ const { assert } = require("chai");
 Given("the user is on login page", async () => {
   try {
     await LoginPage.open();
-    const title = await browser.getTitle();
-    assert.equal(title, "The Internet", "Page title is not valid");
   } catch (error) {
     console.error();
     assert.fail();
@@ -18,8 +16,7 @@ When(
   "user logs in using username as {string} and password as {string}",
   async (username, password) => {
     try {
-      (await LoginPage.txtusername).setValue(username);
-      (await LoginPage.txtpassword).setValue(password);
+      await LoginPage.entervalues(username, password);
     } catch (error) {
       console.error();
       assert.fail();
@@ -29,7 +26,7 @@ When(
 
 Then("user clicks on the login button", async () => {
   try {
-    await LoginPage.btnlogin.click();
+    await LoginPage.clicksignin();
   } catch (error) {
     console.error();
     assert.fail();
@@ -40,18 +37,7 @@ Then(
   "user must navigate to secure area page displaying a message {string}",
   async (successmessage) => {
     try {
-      await (await SecurePage.messageElement).waitForExist();
-      expect(await SecurePage.messageElement).toExist();
-      expect(await SecurePage.messageElement).toHaveTextContaining(
-        successmessage
-      );
-
-      await (await SecurePage.securePageElement).waitForExist();
-      expect(await SecurePage.securePageElement).toExist();
-      expect(await SecurePage.securePageElement).toHaveTextContaining(
-        "Secure Area"
-      );
-      await browser.pause(3000);
+      await LoginPage.logincheck(successmessage);
     } catch (error) {
       console.error();
       assert.fail();
@@ -63,15 +49,7 @@ Then(
   "user must remain on the same login page displaying a message {string}",
   async (errormessage) => {
     try {
-      await (await LoginPage.messageelement).waitForExist();
-      expect(await LoginPage.messageelement).toExist();
-      expect(await LoginPage.messageelement).toHaveTextContaining(errormessage);
-
-      await (await LoginPage.loginpageelement).waitForExist();
-      expect(await LoginPage.loginpageelement).toExist();
-      expect(await LoginPage.loginpageelement).toHaveTextContaining(
-        "Login Page"
-      );
+      await LoginPage.loginerrorcheck(errormessage);
     } catch (error) {
       console.error();
       assert.fail();
